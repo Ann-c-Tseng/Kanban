d3884bd969dd6306aab09fe27ba9106e483de75c
Ann Tseng <anntseng@Anns-MacBook-Pro.local>
2023-11-26 17:35:45 -0800

    add board button persist functions

---
+++ Components/Functionality/boardBtnsPersist.js
@@ -0,0 +1,28 @@
+persistBoardsBtnDisplay();
+
+function persistBoardsBtnDisplay() {
+    if(window.localStorage.getItem("Boards").length !== 0) {
+        displayBtnsExisting();
+    }
+}
+
+function displayBtnsExisting() {
+    var localStorageBoardInfo = JSON.parse(window.localStorage.getItem("Boards"));
+
+    //For each board object, make the button and append to the div
+    for(const e of Object.keys(localStorageBoardInfo)) {
+        var boardObj = localStorageBoardInfo[e];
+        var boardKeys = Object.keys(boardObj);
+        var boardId = boardObj[boardKeys[0]];
+        var boardName = boardObj[boardKeys[1]];
+        var boardColour = boardObj[boardKeys[2]];
+
+        //Each board btn has the following: boardName, boardId, className (boardBtn), style = "--boardBtn-colour: " + boardColour
+        var newBtn = document.createElement("button");
+        newBtn.innerHTML = boardName;
+        newBtn.id = boardId;
+        newBtn.className = 'boardBtn';
+        newBtn.style = "--boardBtn-colour: " + boardColour;
+        document.getElementById("newBoardsDiv").appendChild(newBtn);
+    };
+}
