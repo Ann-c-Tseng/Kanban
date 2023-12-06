0125cbf4bd4e54d10b1dd1e2b87a3477729e40d4
Ann Tseng <anntseng@Anns-MacBook-Pro.local>
2023-12-05 23:23:22 -0800

    adding form styling

--- Components/Functionality/taskAdd.js
+++ Components/Functionality/taskAdd.js
@@ -25,29 +25,56 @@ function newTaskPopup() {
     newTaskForm.id = "newTaskForm";
     document.getElementById("popupDiv").appendChild(newTaskForm);
 
+    var ntfCloseBtn = document.createElement("button");
+    ntfCloseBtn.innerHTML = "Close"
+    ntfCloseBtn.id = "ntfCloseBtn";
+
     var ntfTitle = document.createElement("h3");
     ntfTitle.innerHTML = "Add New Task"
     ntfTitle.id = "ntfTitle";
+
     var ntfInputTitle = document.createElement("input");
     ntfInputTitle.id = "ntfInputTitle";
+    var ntfInputTitleLabel = document.createElement("label");
+    ntfInputTitleLabel.htmlFor = "ntfInputTitle";
+    ntfInputTitleLabel.innerHTML = "Test1";
+
     var ntfInputDescription = document.createElement("input");
     ntfInputDescription.id = "ntfInputDescription";
+    var ntfInputDescriptionLabel = document.createElement("label");
+    ntfInputDescriptionLabel.htmlFor = "ntfInputDescription";
+    ntfInputDescriptionLabel.innerHTML = "Test2";
+
     var ntfInputSubtasks = document.createElement("input");
     ntfInputSubtasks.id = "ntfInputSubtasks";
+    var ntfInputSubtasksLabel = document.createElement("label");
+    ntfInputSubtasksLabel.htmlFor = "ntfInputSubtasks";
+    ntfInputSubtasksLabel.innerHTML = "Test3";
+
     var ntfAddNewSubtaskBtn = document.createElement("button");
     ntfAddNewSubtaskBtn.innerHTML = "Add New Subtask"
     ntfAddNewSubtaskBtn.id = "ntfAddNewSubtaskBtn";
+
     var ntfInputStatus = document.createElement("input");
     ntfInputStatus.id = "ntfInputStatus";
+    var ntfInputStatusLabel = document.createElement("label");
+    ntfInputStatusLabel.htmlFor = "ntfInputStatus";
+    ntfInputStatusLabel.innerHTML = "Test4";
+
     var ntfCreateTaskBtn = document.createElement("button");
     ntfCreateTaskBtn.innerHTML = "Create Task Button"
     ntfCreateTaskBtn.id = "ntfCreateTaskBtn";
 
+    newTaskForm.appendChild(ntfCloseBtn);
     newTaskForm.appendChild(ntfTitle);
+    newTaskForm.appendChild(ntfInputTitleLabel);
     newTaskForm.appendChild(ntfInputTitle);
+    newTaskForm.appendChild(ntfInputDescriptionLabel);
     newTaskForm.appendChild(ntfInputDescription);
+    newTaskForm.appendChild(ntfInputSubtasksLabel);
     newTaskForm.appendChild(ntfInputSubtasks);
     newTaskForm.appendChild(ntfAddNewSubtaskBtn);
+    newTaskForm.appendChild(ntfInputStatusLabel);
     newTaskForm.appendChild(ntfInputStatus);
     newTaskForm.appendChild(ntfCreateTaskBtn);
 }
