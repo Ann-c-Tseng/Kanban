da165127118c4d48a8c2edaca91f8338d9bfae15
--unset <ann.c.tseng@gmail.com>
2023-12-13 23:03:05 -0800

    finish up the new list popup form

--- Components/Functionality/addNewList.js
+++ Components/Functionality/addNewList.js
@@ -15,9 +15,23 @@ function newListPopup() {
     newListForm.id = "newListForm";
     document.getElementById("listPopupDiv").appendChild(newListForm);
 
+    var nlfTitle = document.createElement("h3");
+    nlfTitle.innerHTML = "Add New List";
+    nlfTitle.id = "nlfTitle";
+
+    var nlfInputListName = document.createElement("input");
+    nlfInputListName.id = "nlfInputTitle";
+    var nlfInputListNameLabel = document.createElement("label");
+    nlfInputListNameLabel.id = "nlfInputListNameLabel";
+    nlfInputListNameLabel.htmlFor = "nlfInputListName";
+    nlfInputListNameLabel.innerHTML = "List Name";
+
     var nlfCloseBtn = document.createElement("button");
     nlfCloseBtn.innerHTML = "Close"
     nlfCloseBtn.id = "nlfCloseBtn";
 
+    newListForm.appendChild(nlfTitle);
+    newListForm.appendChild(nlfInputListNameLabel);
+    newListForm.appendChild(nlfInputListName);
     newListForm.appendChild(nlfCloseBtn);
 }
