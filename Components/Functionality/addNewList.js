77d7b5ba1b88215bb17514820b13cbf528b141d9
--unset <ann.c.tseng@gmail.com>
2023-12-16 23:16:17 -0800

    starting save and close event functions for adding new lists from popup

--- Components/Functionality/addNewList.js
+++ Components/Functionality/addNewList.js
@@ -25,6 +25,9 @@ function newListPopup() {
     nlfInputListNameLabel.id = "nlfInputListNameLabel";
     nlfInputListNameLabel.htmlFor = "nlfInputListName";
     nlfInputListNameLabel.innerHTML = "List Name";
+    var nlfSaveBtn = document.createElement("button");
+    nlfSaveBtn.innerHTML = "Save";
+    nlfSaveBtn.id = "nlfSaveBtn";
 
     var nlfCloseBtn = document.createElement("button");
     nlfCloseBtn.innerHTML = "Close"
@@ -33,5 +36,24 @@ function newListPopup() {
     newListForm.appendChild(nlfTitle);
     newListForm.appendChild(nlfInputListNameLabel);
     newListForm.appendChild(nlfInputListName);
+    newListForm.appendChild(nlfSaveBtn);
     newListForm.appendChild(nlfCloseBtn);
+
+    addClickEventToListPopup("nlfSaveBtn", "nlfCloseBtn");
+}
+
+function addClickEventToListPopup(sBtnId, cBtnId) {
+    var saveBtn = document.getElementById(sBtnId);
+    var closeBtn = document.getElementById(cBtnId);
+
+    saveBtn.addEventListener("click", saveEvent);
+    closeBtn.addEventListener("click", closeEvent);
+}
+
+function saveEvent(evt) {
+    evt.preventDefault();
+}
+
+function closeEvent(evt) {
+    evt.preventDefault();
 }
